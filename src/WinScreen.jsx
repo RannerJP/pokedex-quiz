685b9d7dd076a9a301a61fa216d82fe0b8127ca8
Jose Pavon <RannerJP@gmail.com>
2025-05-01 21:40:22 -0700

    Bold the score for emphasis, added a return to homescreen button

--- src/WinScreen.jsx
+++ src/WinScreen.jsx
@@ -1,14 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import "./WinScreen.css";
+
 
 function WinScreen() {
     const { totalScore } = useParams();
 
 
     return (
-        <>
+        <div className="WinScreen">
             {totalScore && (
-                <div> Your score was {totalScore} congrats!</div>)}
-        </>
+                <p> Your total score is <strong>{totalScore}</strong> congratulations!</p>)}
+                <Link to="/pokedex-quiz" className='linkButton'>Return to Homepage</Link>
+        </div>
     );
 }
 
